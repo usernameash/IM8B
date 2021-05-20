9e8265dbd2b3069a517ba946588956fb66be42fe
ash <ashy3k@gmail.com>
2021-05-20 17:57:07 +0100

    Improved the answer generation

--- src/IM8B/Program.cs
+++ src/IM8B/Program.cs
@@ -12,8 +12,8 @@ namespace IM8B
         static void Main(string[] args)
         {
 
-        int rannum = 0;
-        Random r = new Random();
+            int rannum = 0;
+            Random r = new Random();
 
             //Ask user for name
             Console.Clear();
@@ -38,8 +38,8 @@ namespace IM8B
             Console.WriteLine($"You are named {name}? That is a very cool name!");
             Thread.Sleep(2000);
             Console.Clear();
-            System.Console.WriteLine($"So {name}, What is it you want me to answer?");
-            System.Console.WriteLine("Please remember that I can only answer yes or no typed questions:");
+            Console.WriteLine($"So {name}, What is it you want me to answer?");
+            Console.WriteLine("Please remember that I can only answer yes or no typed questions:");
 
             //Makes sure user asks a valid question
             string question = Console.ReadLine();
@@ -54,99 +54,39 @@ namespace IM8B
                 Console.WriteLine("...and yet, you've slipped into the painting?");
                 Thread.Sleep(2500);
                 Console.Clear();
-                System.Console.WriteLine($"So {name}, What is it you want me to answer?");
-                System.Console.WriteLine("Please remember that I can only answer yes or no questions");
+                Console.WriteLine($"So {name}, What is it you want me to answer?");
+                Console.WriteLine("Please remember that I can only answer yes or no questions");
                 Console.ReadLine();
             }
 
-        //Below is the random num generator and its possible outcomes (Answers from the Magic 8 Ball)
-        rannum = r.Next(1,20);
+            var answers = new List<string>(){
+                "It is Certain",
+                "It is decidedly so",
+                "Without a doubt",
+                "Yes definitely",
+                "You may rely on it",
+                "As I see it, yes",
+                "Most likely",
+                "Outlook good",
+                "Yes",
+                "Signs point to yes",
+                "Reply hazy, try again",
+                "Ask again later",
+                "Better not tell you now",
+                "Cannot predict now",
+                "Concentrate and try again",
+                "Don't count on it",
+                "My reply is no",
+                "My sources say no",
+                "Outlook not so good",
+                "Very doubtful",
+            };
+            //Below is the random num generator and its possible outcomes (Answers from the Magic 8 Ball
+            rannum = r.Next(0, answers.Count);
 
-        switch (rannum)
-        {
-            case 1:
-            Console.WriteLine("It is Certain");
-            break;
+            Console.WriteLine(answers[rannum]);
 
-            case 2:
-            Console.WriteLine("It is decidedly so");
-            break;
-
-            case 3:
-            Console.WriteLine("Without a doubt");
-            break;
-
-            case 4:
-            Console.WriteLine("Yes definitely");
-            break;
-
-            case 5:
-            Console.WriteLine("You may rely on it");
-            break;
-            
-            case 6:
-            Console.WriteLine("As I see it, yes");
-            break;
-            
-            case 7:
-            Console.WriteLine("Most likely");
-            break;
-            
-            case 8:
-            Console.WriteLine("Outlook good");
-            break;
-
-            case 9:
-            Console.WriteLine("Yes");
-            break;
-
-            case 10:
-            Console.WriteLine("Signs point to yes");
-            break;
-
-            case 11:
-            Console.WriteLine("Reply hazy, try again");
-            break;
-
-            case 12:
-            Console.WriteLine("Ask again later");
-            break;
-
-            case 13:
-            Console.WriteLine("Better not tell you now");
-            break;
-
-            case 14:
-            Console.WriteLine("Cannot predict now");
-            break;
-
-            case 15:
-            Console.WriteLine("Concentrate and try again");
-            break;
-
-            case 16:
-            Console.WriteLine("Don't count on it");
-            break;
-
-            case 17:
-            Console.WriteLine("My reply is no");
-            break;
-
-            case 18:
-            Console.WriteLine("My sources say no");
-            break;
-
-            case 19:
-            Console.WriteLine("Outlook not so good");
-            break;
-
-            case 20:
-            Console.WriteLine("Very doubtful");
-            break;
-
-        }
-
-        Console.ReadLine();
+            Console.ReadLine();
         }
     }
 }
